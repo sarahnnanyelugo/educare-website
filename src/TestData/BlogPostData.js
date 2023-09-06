474f72aa1f15763db2ec8d56cdf9e13b70a62a41
Sarah <sarahnnanyelugo@gmail.com>
2023-09-06 09:06:07 +0100

    updates blog page

--- src/TestData/BlogPostData.js
+++ src/TestData/BlogPostData.js
@@ -51,21 +51,21 @@ export const blogPostColOne = [
     blog_id: 5,
     date: "2022-01-03T12:00:00",
   },
-  {
-    title:
-      "Why Schools Should Consider Adopting A Data-Driven Culture Now More Than Ever",
-    paragraph:
-      "Start with data-driven decision making. I heard it all the time when I was an undergrad: Be a “data-driven decision maker.” No matter ho…",
-    img: Img6,
-    blog_id: 6,
-    date: "2022-12-03T12:00:00",
-  },
-  {
-    title: "Top 24 Educational Trends For 2020",
-    paragraph:
-      "Every year we are seeing a change of trends in almost all sectors and the case is no different in the field of education. This is one of the areas we s…",
-    img: Img7,
-    blog_id: 7,
-    date: "2020-12-03T12:00:00",
-  },
+  // {
+  //   title:
+  //     "Why Schools Should Consider Adopting A Data-Driven Culture Now More Than Ever",
+  //   paragraph:
+  //     "Start with data-driven decision making. I heard it all the time when I was an undergrad: Be a “data-driven decision maker.” No matter ho…",
+  //   img: Img6,
+  //   blog_id: 6,
+  //   date: "2022-12-03T12:00:00",
+  // },
+  // {
+  //   title: "Top 24 Educational Trends For 2020",
+  //   paragraph:
+  //     "Every year we are seeing a change of trends in almost all sectors and the case is no different in the field of education. This is one of the areas we s…",
+  //   img: Img7,
+  //   blog_id: 7,
+  //   date: "2020-12-03T12:00:00",
+  // },
 ];
--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -10,6 +10,13 @@ import { Heading } from "../../components/Heading/Heading";
 import FocusedBlog from "./BlogPost/FocusedBlog";
 
 function Blog() {
+  const [activeIndex, setActiveIndex2] = useState(1);
+  const handleClick2 = (index) => setActiveIndex2(index);
+  const checkActive = (index, className) =>
+    activeIndex === index ? className : "";
+  const checkActive2 = (index, className) =>
+    activeIndex === index ? className : "";
+
   const [state, setState] = useState({
     query: "",
     list: blogPostColOne,
@@ -39,33 +46,62 @@ function Blog() {
           </div>
         </div>
       </div>
-
-      <div className="col-md-10 offset-md-1 ">
-        <div className="bold-heading">
-          {" "}
-          <h2>Schools</h2>
+      <div className="blog-tab">
+        <div className="tabs offset-md-1">
+          <button
+            className={`tab ${checkActive2(1, "active2")}`}
+            onClick={() => handleClick2(1)}
+          >
+            Schools
+          </button>
+          <button
+            className={`tab ${checkActive2(2, "active2")}`}
+            onClick={() => handleClick2(2)}
+            style={{ marginLeft: "20px" }}
+          >
+            Businesses
+          </button>{" "}
+          <button
+            className={`tab ${checkActive2(3, "active2")}`}
+            onClick={() => handleClick2(3)}
+            style={{ marginLeft: "20px" }}
+          >
+            News & Events
+          </button>
+        </div>
+        <div className="panels">
+          <div className={`panel ${checkActive(1, "active2")}`}>
+            <div className="col-md-10 offset-md-1 ">
+              <div className="bold-heading">
+                {" "}
+                <h2>Schools</h2>
+              </div>
+            </div>
+            <div className="col-md-10 offset-md-1  mt">
+              <div className="row ">
+                {state2.list2.map((data, index) => (
+                  <FocusedBlog data={data} />
+                ))}
+              </div>
+            </div>
+            <div className="col-md-10 offset-md-1  mt">
+              <div className="row ">
+                {state.list.map((data, index) => (
+                  <MiniBlog data={data} />
+                ))}
+              </div>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+          </div>
+          <div className={`panel ${checkActive(2, "active2")}`}>2</div>
+          <div className={`panel ${checkActive(3, "active2")}`}>3</div>
         </div>
       </div>
-      <div className="col-md-10 offset-md-1  mt">
-        <div className="row ">
-          {state2.list2.map((data, index) => (
-            <FocusedBlog data={data} />
-          ))}
-        </div>
-      </div>
-      <div className="col-md-10 offset-md-1  mt">
-        <div className="row ">
-          {state.list.map((data, index) => (
-            <MiniBlog data={data} />
-          ))}
-        </div>
-      </div>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
     </>
   );
 }
