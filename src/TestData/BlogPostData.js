4ae2ddbd400e4bfb0b87949acd6400553f1c0a27
Sarah <sarahnnanyelugo@gmail.com>
2023-09-08 07:59:45 +0100

    blog

--- src/TestData/BlogPostData.js
+++ src/TestData/BlogPostData.js
@@ -8,48 +8,57 @@ import Img6 from "../assets/images/office-view.png";
 
 export const blogPostColOne = [
   {
-    title:
-      "We Must Equip Teachers With The Skills And Knowledge They Need To Be Confident And Effective Users Of Technology",
+    title: "Human Resource Management (HRM)",
     paragraph:
-      "Every year we are seeing a change of trends in almost all sectors and the case is no different in the field of education. This is one of the are…",
+      "We’re excited to announce the latest feature on our educare e-portal. That is the educare Human Resources Manager (HRM) Feature. It aims to ha…",
     img: Img1,
     date: "2022-12-03T12:00:00",
     blog_id: 1,
+    bloggerTitle: "Head of Support",
+    blogger: "Henry Okafor",
   },
   {
-    title:
-      "Using educare LMS To Keep Up With The Pace Of Growth And Student Demand",
+    title: "Top 24 Educational Trends For 2020",
     paragraph:
-      "Challenge An LMS to facilitate online learning to handle rapid school growth and changing needs Products & Solutions educ…",
+      "Every year we are seeing a change of trends in almost all sectors and the case is no different in the field of education. This is one of the areas we s…",
     img: Img3,
     blog_id: 2,
     date: "2023-12-03T12:00:00",
+    bloggerTitle: "Head of Support",
+    blogger: "Henry Okafor",
   },
   {
-    title: "Teachers Guide On How To Set CBT Questions",
+    title:
+      "Why Schools Should Consider Adopting A Data-Driven Culture Now More Than Ever",
     paragraph:
-      "Follow the steps below to set up questions for your exam; STEP 1: As indicated in the picture below, click on the “CBT” dropdow…",
+      "Start with data-driven decision making. I heard it all the time when I was an undergrad: Be a “data-driven decision maker.” No matter ho…",
     img: Img4,
     blog_id: 3,
     date: "2021-12-03T12:00:00",
+    bloggerTitle: "Head of Support",
+    blogger: "Henry Okafor",
   },
   {
-    title: "Human Resource Management (HRM)",
+    title:
+      "Using Educare LMS To Keep Up With The Pace Of Growth And Student Demand",
     paragraph:
       "We’re excited to announce the latest feature on our educare e-portal. That is the educare Human Resources Manager (HRM) Feature. It aims to har…",
     img: Img5,
     blog_id: 4,
+    bloggerTitle: "Head of Support",
+    blogger: "Henry Okafor",
     date: "2020-12-03T12:00:00",
   },
 
   {
-    title:
-      "K-12: What Are The Pedagogical Advantages Of Online Learning, Compared To In-person Classroom Learning?",
+    title: "Teachers Guide On How To Set CBT Questions",
     paragraph:
-      "“Online learning can be as good or even better than in-person classroom learning. Research has shown that students in online lear…",
+      "Follow the steps below to set up questions for your exam; STEP 1: As indicated in the picture below, click on the “CBT” dropdow…",
     img: Img2,
     blog_id: 5,
     date: "2022-01-03T12:00:00",
+    bloggerTitle: "Head of Support",
+    blogger: "Henry Okafor",
   },
   // {
   //   title:
--- src/pages/Blog/BlogPost/MiniBlog.js
+++ src/pages/Blog/BlogPost/MiniBlog.js
@@ -11,15 +11,23 @@ function MiniBlog({ data }) {
   });
   return (
     <>
-      <div className="col-md-4">
+      <div className="col-md-3">
         {" "}
         <div className="blog-post-container col-md-11 ">
           <img className="" src={data.img} alt="Scholar" width="100%" />
           <div className="blog-post-container-inner">
-            {" "}
+            <p className="">{data.date}</p>
             <h5>{data.title}</h5>
             <p>{data.paragraph}</p>
-            <div className="flexy flexyM">
+
+            <div className="flexy">
+              <img className="2 " src={Arrow} alt="Scholar" width="10%" />
+              <div>
+                <h6>{data.blogger}</h6>
+                <p>{data.bloggerTitle}</p>
+              </div>
+            </div>
+            {/* <div className="flexy flexyM">
               <Link
                 to="/main-blog"
                 state={{ blog_id: blogId }}
@@ -34,7 +42,22 @@ function MiniBlog({ data }) {
                 />
               </Link>
               <p className="mt3">{data.date}</p>
-            </div>
+            </div> {/* <div className="flexy flexyM">
+              <Link
+                to="/main-blog"
+                state={{ blog_id: blogId }}
+                className="blog-link"
+              >
+                Read post{" "}
+                <img
+                  className="col-md-12 "
+                  src={Arrow}
+                  alt="Scholar"
+                  width="10%"
+                />
+              </Link>
+         
+            </div> */}
           </div>
         </div>
       </div>
